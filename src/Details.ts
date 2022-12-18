7671cd65376f74b85d890462d1167d384918f844
Rustam Kot <ruskir7@gmail.com>
2022-12-18 16:00:08 +0300

    feat: added details to have 1 entrance point

---
+++ src/Details.ts
@@ -0,0 +1,184 @@
+/* eslint-disable simple-import-sort/imports */
+// Enter all your detials in this file
+// Logo images
+// import logogradient from './assets/logo.svg';
+// import logo from './assets/logo2.svg';
+// Profile Image
+import profile from './assets/images/SimpleAvatar.png';
+// Tech stack images
+import HTML5 from './assets/images/HTML5-logo.png';
+import CSS3 from './assets/images/CSS3-logo.png';
+import JavaScript from './assets/images/JavaScript-logo.png';
+import React from './assets/images/React-logo.png';
+import Typescript from './assets/images/Typescript_logo.svg';
+import Redux from './assets/images/redux-logo.png';
+import Tailwind from './assets/images/Tailwind_CSS-logo.png';
+import HelloPangea from './assets/images/hello-pangea_dnd-logo.png';
+import CRA from './assets/images/CRA-logo.png';
+import Vite from './assets/images/vite-logo.png';
+// Tools
+import VSCode from './assets/images/VSCode-logo.png';
+import Git from './assets/images/Git-logo.png';
+import GitHub from './assets/images/github-logo.png';
+import Npm from './assets/images/Npm-logo.png';
+import Postman from './assets/images/postman-logo.png';
+// Porject Images
+import projectImage1 from './assets/projects/project1.jpg';
+import projectImage2 from './assets/projects/project2.jpg';
+import projectImage3 from './assets/projects/project3.jpg';
+import projectImage4 from './assets/projects/project4.jpg';
+import projectImage5 from './assets/projects/project5.jpg';
+import projectImage6 from './assets/projects/project6.jpg';
+
+// Logos
+// export const logos = {
+//   logogradient: logogradient,
+//   logo: logo,
+// };
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: 'Rustam',
+  tagline: 'I build things for web',
+  img: profile,
+  about: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  github: 'https://www.github.com/bycolour2',
+  discord: 'https://discordapp.com/users/135416426369515520',
+  telegram: 'https://t.me/bycolour',
+  linkdein: '',
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    position: 'Frontend Web Developer',
+    company: `Company Name here`,
+    location: 'Bengaluru',
+    type: 'Full Time',
+    duration: 'Sep 2021 - Dec 2021',
+  },
+  {
+    position: 'Internship',
+    company: `Company Name here`,
+    location: 'Bengaluru',
+    type: 'Internship',
+    duration: 'Sep 2021 - Dec 2021',
+  },
+  {
+    position: 'Internship',
+    company: `Company Name here`,
+    location: 'Bengaluru',
+    type: 'Internship',
+    duration: 'Sep 2021 - Dec 2021',
+  },
+];
+
+// Enter your Education Details here
+export const educationDetails = [
+  {
+    position: 'Frontend Development',
+    company: 'Udemy, YouTube, Google, Medium',
+    location: 'Online',
+    type: 'Full Time',
+    duration: 'Jan 2022 - Present',
+  },
+  {
+    position: 'Bachelor in Electronics & Communication',
+    company: `Your College Name here`,
+    location: 'Bengaluru',
+    type: 'Full Time',
+    duration: 'Aug 2020 - Present',
+  },
+];
+
+// Tech Stack
+export const techStackDetails = {
+  html: HTML5,
+  css: CSS3,
+  js: JavaScript,
+  react: React,
+  typescript: Typescript,
+  redux: Redux,
+  tailwind: Tailwind,
+  helloPangea: HelloPangea,
+  cra: CRA,
+  vite: Vite,
+};
+
+// Tools
+export const toolsDetails = {
+  vscode: VSCode,
+  git: Git,
+  github: GitHub,
+  npm: Npm,
+  postman: Postman,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: 'Project title 1',
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: 'HTML/CSS, JavaScript',
+    previewLink: 'https://google.com',
+    githubLink: 'https://github.com',
+  },
+  {
+    title: 'Project title 2',
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: 'HTML/CSS, JavaScript',
+    previewLink: 'https://google.com',
+    githubLink: 'https://github.com',
+  },
+  {
+    title: 'Project title 3',
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: 'HTML/CSS, JavaScript',
+    previewLink: 'https://google.com',
+    githubLink: 'https://github.com',
+  },
+  {
+    title: 'Project title 4',
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: 'HTML/CSS, JavaScript',
+    previewLink: 'https://google.com',
+    githubLink: 'https://github.com',
+  },
+  {
+    title: 'Project title 5',
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: 'HTML/CSS, JavaScript',
+    previewLink: 'https://google.com',
+    githubLink: 'https://github.com',
+  },
+  {
+    title: 'Project title 6',
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: 'HTML/CSS, JavaScript',
+    previewLink: 'https://google.com',
+    githubLink: 'https://github.com',
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: 'ruskir7@gmail.com',
+  telegram: '@bycolour',
+  phone: '',
+};
