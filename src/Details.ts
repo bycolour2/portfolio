8b7fc0c244480ef44a478ad59b6e023ed030ffc2
bycolour2 <ruskir7@gmail.com>
2023-11-23 16:55:20 +0300

    feat: updated personal info

--- src/Details.ts
+++ src/Details.ts
@@ -16,6 +16,9 @@ import Tailwind from './assets/images/Tailwind_CSS-logo.png';
 import HelloPangea from './assets/images/hello-pangea_dnd-logo.png';
 import CRA from './assets/images/CRA-logo.png';
 import Vite from './assets/images/vite-logo.png';
+import RHF from './assets/images/rhf-logo.png';
+import Effector from './assets/images/effector-logo.png';
+import FramerMotion from './assets/images/framer-motion-logo.png';
 // Tools
 import VSCode from './assets/images/VSCode-logo.png';
 import Git from './assets/images/Git-logo.png';
@@ -25,10 +28,10 @@ import Postman from './assets/images/postman-logo.png';
 // Porject Images
 import projectImage1 from './assets/projects/convertingInput.png';
 import projectImage2 from './assets/projects/rocket-pma.png';
-import projectImage3 from './assets/projects/portfolio.png';
-import projectImage4 from './assets/projects/project4.jpg';
-import projectImage5 from './assets/projects/project5.jpg';
-import projectImage6 from './assets/projects/project6.jpg';
+import projectImage3 from './assets/projects/dartsGames.png';
+// import projectImage4 from './assets/projects/project4.jpg';
+// import projectImage5 from './assets/projects/project5.jpg';
+// import projectImage6 from './assets/projects/project6.jpg';
 // Certificates
 import RSSReactQ3Cert from './assets/pdfs/RSS React 2022 Q3 Cert.pdf';
 
@@ -57,11 +60,19 @@ export const socialMediaUrl = {
 // Enter your Work Experience here
 export const workDetails = [
   {
+    position:
+      'Head of the development and maintenance group of internal information systems of the development and implementation department of the IMS',
+    company: `Gazprom Procurement LLC`,
+    location: 'St. Petersburg',
+    type: 'Full Time',
+    duration: 'Oct 2023 - Present',
+  },
+  {
     position: 'Senior Specialist of the IMS Development and Implementation Department',
     company: `Gazprom Procurement LLC`,
     location: 'St. Petersburg',
     type: 'Full Time',
-    duration: 'Feb 2023 - Present',
+    duration: 'Feb 2023 - Sep 2023',
   },
   {
     position:
@@ -128,6 +139,9 @@ export const techStackDetails = {
   helloPangea: HelloPangea,
   cra: CRA,
   vite: Vite,
+  RHF: RHF,
+  effector: Effector,
+  framerMotion: FramerMotion,
 };
 
 // Tools
@@ -156,47 +170,48 @@ export const projectDetails = [
     image: projectImage2,
     description: `Project management system created as final task for RSSchool course. Application uses React-router-dom, redux, websocket, drag and drop and other simplier technologies.`,
     techstack:
-      'React, TypeScript, CRA, Tailwindcss, Redux, RTK, Websocket, Pangea-DnD (ex. Beautiful DnD), RRD, Flowbite',
+      'React, TypeScript, CRA, Tailwindcss, Redux, RTK, Websocket, Pangea-DnD (ex. Beautiful DnD), react-router-dom, Flowbite',
     previewLink: 'https://rocket-pma.onrender.com/',
     githubLink: 'https://github.com/bycolour2/project-management-app',
   },
   {
-    title: 'Portfolio',
+    title: 'Darts games',
     image: projectImage3,
-    description: `My portfolio, as you can see :)`,
-    techstack: 'React, TypeScript, Vite, Tailwindcss, Framer Motion',
-    previewLink: 'https://bycolour2.github.io/portfolio/',
-    githubLink: 'https://github.com/bycolour2/portfolio',
+    description: `🚧 Work in progress 🚧. Project created as Darts lover to record some game data.`,
+    techstack:
+      'React, TypeScript, Vite, Tailwindcss, Framer Motion, effector, shadcn, FSD',
+    previewLink: 'https://dartsgames.onrender.com/',
+    githubLink: 'https://github.com/bycolour2/darts-games/tree/main',
   },
-  {
-    title: 'Project title 4',
-    image: projectImage4,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: 'HTML/CSS, JavaScript',
-    previewLink: 'https://google.com',
-    githubLink: 'https://github.com',
-  },
-  {
-    title: 'Project title 5',
-    image: projectImage5,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: 'HTML/CSS, JavaScript',
-    previewLink: 'https://google.com',
-    githubLink: 'https://github.com',
-  },
-  {
-    title: 'Project title 6',
-    image: projectImage6,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content. This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content. This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: 'HTML/CSS, JavaScript',
-    previewLink: 'https://google.com',
-    githubLink: 'https://github.com',
-  },
+  //   {
+  //     title: 'Project title 4',
+  //     image: projectImage4,
+  //     description: `This is sample project description random things are here in description This is sample
+  // project lorem ipsum generator for dummy content`,
+  //     techstack: 'HTML/CSS, JavaScript',
+  //     previewLink: 'https://google.com',
+  //     githubLink: 'https://github.com',
+  //   },
+  //   {
+  //     title: 'Project title 5',
+  //     image: projectImage5,
+  //     description: `This is sample project description random things are here in description This is sample
+  // project lorem ipsum generator for dummy content`,
+  //     techstack: 'HTML/CSS, JavaScript',
+  //     previewLink: 'https://google.com',
+  //     githubLink: 'https://github.com',
+  //   },
+  //   {
+  //     title: 'Project title 6',
+  //     image: projectImage6,
+  //     description: `This is sample project description random things are here in description This is sample
+  // project lorem ipsum generator for dummy content. This is sample project description random things are here in description This is sample
+  // project lorem ipsum generator for dummy content. This is sample project description random things are here in description This is sample
+  // project lorem ipsum generator for dummy content`,
+  //     techstack: 'HTML/CSS, JavaScript',
+  //     previewLink: 'https://google.com',
+  //     githubLink: 'https://github.com',
+  //   },
 ];
 
 // Enter your Contact Details here
